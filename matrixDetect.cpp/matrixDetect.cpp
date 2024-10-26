2da8e5e0f21b180ca9015c659feded8142f17540
Omar Onate <119543989+OO101@users.noreply.github.com>
2024-10-26 10:18:33 -0700

    Update matrixDetect.cpp

--- matrixDetect.cpp/matrixDetect.cpp
+++ matrixDetect.cpp/matrixDetect.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <iomanip>
 using namespace std;
-
+//Initalize the varibles.
 string userFile, line, temp;
 int row = 0;
 int col = 0;
@@ -12,22 +12,30 @@ int num;
 
 int main()
 {
+	//Ask user to input file name (HW15_1.txt).
 	cout << "Enter file name: ";
 	getline(cin, userFile);
 	cout << "Now reading " << userFile << "..." << endl;
-	
+
+	//Initalize inputFile varible to take in & open the userFile.
 	ifstream inputFile;
 	inputFile.open(userFile);
 
 	
-
+//Keep iterating while there is still lines to get within the file.
 	while(getline(inputFile, line))
 	{
+	//Increment the amount of rows within the file.
 		row++;
+
+	//Check if row is equal to 1.
 		if (row == 1)
 		{
+	//If it is then initalize ss to take in the line.
 			stringstream ss(line);
+	//Keep iterating while ss keeps shifting the bits to temp.
 			while(ss >> temp)
+		//Increment the columns within the file.
 				col++;
 		}
 	}
